791c96b773dcb21965421db4e122bec34841fa8d
Thauã <thaua23sl@gmail.com>
2026-01-03 09:43:59 -0300

    add social midia imgs

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Detecta se a tela é 4K ou maior
-    const is4K = window.innerWidth >= 3840;
+   // Detecta se a tela é 4K ou maior
+   const is4K = window.innerWidth >= 3840;
 
-    tsParticles.load("particles-container", {
-        fpsLimit: 60,
-        interactivity: {
-            events: {
-                onHover: {
-                    enable: true,
-                    mode: "repulse",
-                },
-                onClick: {
-                    enable: false,
-                    mode: "push",
-                },
-                resize: true,
+   tsParticles.load("particles-container", {
+      fpsLimit: 60,
+      interactivity: {
+         events: {
+            onHover: {
+               enable: true,
+               mode: "repulse",
             },
-            modes: {
-                push: { quantity: 4 },
-                repulse: { distance: 100, duration: 0.4 },
+            onClick: {
+               enable: false,
+               mode: "push",
             },
-        },
-        particles: {
-            color: { value: "#ffffff" },
-            links: {
-                color: "#a1a1a1",
-                distance: 150,
-                enable: true,
-                opacity: 0.4,
-                width: 1,
-            },
-            collisions: { enable: true },
-            move: {
-                direction: "none",
-                enable: true,
-                outModes: { default: "bounce" },
-                random: false,
-                speed: 1,
-                straight: false,
-            },
-            number: {
-                density: { enable: true, area: 800 },
-                value: is4K ? 40 : 80, // menos partículas em 4K
-            },
-            opacity: { value: 0.5 },
-            shape: { type: "circle" },
-            size: {
-                value: is4K ? { min: 1, max: 3 } : { min: 1, max: 5 }, // partículas menores em 4K
-            },
-        },
-        detectRetina: true,
-    });
+            resize: true,
+         },
+         modes: {
+            push: { quantity: 4 },
+            repulse: { distance: 100, duration: 0.4 },
+         },
+      },
+      particles: {
+         color: { value: "#ffffff" },
+         links: {
+            color: "#a1a1a1",
+            distance: 150,
+            enable: true,
+            opacity: 0.4,
+            width: 1,
+         },
+         collisions: { enable: true },
+         move: {
+            direction: "none",
+            enable: true,
+            outModes: { default: "bounce" },
+            random: false,
+            speed: 1,
+            straight: false,
+         },
+         number: {
+            density: { enable: true, area: 800 },
+            value: is4K ? 40 : 80, // menos partículas em 4K
+         },
+         opacity: { value: 0.5 },
+         shape: { type: "circle" },
+         size: {
+            value: is4K ? { min: 1, max: 3 } : { min: 1, max: 5 }, // partículas menores em 4K
+         },
+      },
+      detectRetina: true,
+   });
 });
 
 
-//projects slider
+//* projects slider
 
 let nextBtn = document.getElementById("next");
 let prevBtn = document.getElementById("prev");
@@ -63,76 +63,75 @@ let pageNumbers = document.getElementById("page-numbers");
 let slideIndex = 0;
 
 const projects = [
-    [
-        "https://thaua23.github.io/THaua23-Starbucks-landing-page/",
-        "https://thaua23.github.io/car/",
-        "https://thaua23.github.io/THaua23-THaua23-Landing-page-Newslatter/",
-        "https://thaua23.github.io/starbucks/",
-        "https://thaua23.github.io/SCM/",
-        "https://thaua23.github.io/refri/"
-    ],
-    [
-        "https://thaua23.github.io/Sistema-Mercado/",
-        "https://thaua23.github.io/todolist/",
-        "https://thaua23.github.io/TaskBoard/",
-        "https://thaua23.github.io/calculator/",
-        "https://thaua23.github.io/jump/",
-        "https://thaua23.github.io/form/"
-    ],
-    [
-        "https://thaua23.github.io/voice/",
-        "https://thaua23.github.io/penJS/"
-    ]
+   [
+      "https://thaua23.github.io/THaua23-Starbucks-landing-page/",
+      "https://thaua23.github.io/car/",
+      "https://thaua23.github.io/THaua23-THaua23-Landing-page-Newslatter/",
+      "https://thaua23.github.io/starbucks/",
+      "https://thaua23.github.io/SCM/",
+      "https://thaua23.github.io/refri/"
+   ],
+   [
+      "https://thaua23.github.io/Sistema-Mercado/",
+      "https://thaua23.github.io/todolist/",
+      "https://thaua23.github.io/TaskBoard/",
+      "https://thaua23.github.io/calculator/",
+      "https://thaua23.github.io/jump/",
+      "https://thaua23.github.io/form/"
+   ],
+   [
+      "https://thaua23.github.io/voice/",
+      "https://thaua23.github.io/penJS/"
+   ]
 ];
 
 function updateProjects() {
-    projectsBox.innerHTML = "";
+   projectsBox.innerHTML = "";
 
-    projects[slideIndex].forEach(url => {
-        const link = document.createElement("a");
-        link.href = url;
-        link.target = "_blank";
-        link.className = "project-link";
+   projects[slideIndex].forEach(url => {
+      const link = document.createElement("a");
+      link.href = url;
+      link.target = "_blank";
+      link.className = "project-link";
 
-        const iframe = document.createElement("iframe");
-        iframe.src = url;
-        iframe.frameBorder = "0";
+      const iframe = document.createElement("iframe");
+      iframe.src = url;
+      iframe.frameBorder = "0";
 
-        link.appendChild(iframe);
-        projectsBox.appendChild(link);
-    });
+      link.appendChild(iframe);
+      projectsBox.appendChild(link);
+   });
 
-    updatePageButtons();
+   updatePageButtons();
 }
 
 function updatePageButtons() {
-    pageNumbers.innerHTML = "";
+   pageNumbers.innerHTML = "";
 
-    projects.forEach((_, index) => {
-        const btn = document.createElement("button");
-        btn.textContent = index + 1;
-        btn.className = index === slideIndex ? "active" : "";
-        btn.addEventListener("click", () => {
-            slideIndex = index;
-            updateProjects();
-        });
-        pageNumbers.appendChild(btn);
-    });
+   projects.forEach((_, index) => {
+      const btn = document.createElement("button");
+      btn.textContent = index + 1;
+      btn.className = index === slideIndex ? "active" : "";
+      btn.addEventListener("click", () => {
+         slideIndex = index;
+         updateProjects();
+      });
+      pageNumbers.appendChild(btn);
+   });
 }
 
 nextBtn.addEventListener("click", () => {
-    slideIndex = (slideIndex + 1) % projects.length;
-    updateProjects();
+   slideIndex = (slideIndex + 1) % projects.length;
+   updateProjects();
 });
 
 prevBtn.addEventListener("click", () => {
-    slideIndex = (slideIndex - 1 + projects.length) % projects.length;
-    updateProjects();
+   slideIndex = (slideIndex - 1 + projects.length) % projects.length;
+   updateProjects();
 });
 
 // Inicializa
 updateProjects();
-
 
 //* contato
 
@@ -142,48 +141,72 @@ let mensagem = document.getElementById("message");
 let botao = document.getElementById("send-button");
 
 function formatarMensagem() {
-    return `Nome: ${nome.value}%0AEmail: ${email.value}%0AMensagem: ${mensagem.value}`;
+   return `Nome: ${nome.value}%0AEmail: ${email.value}%0AMensagem: ${mensagem.value}`;
 }
 
 botao.addEventListener("click", function () {
-    let linkEmail = "mailto:thaua23sl@gmail.com?subject=Contato%20pelo%20site&body=" + formatarMensagem();
+   let linkEmail = "mailto:thaua23sl@gmail.com?subject=Contato%20pelo%20site&body=" + formatarMensagem();
 
-    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
+   const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
 
-    if (nome.value.trim() !== '' && email.value.trim() !== '' && emailValido) {
-        window.location.href = linkEmail;
-        [nome, email, mensagem].forEach(campo => campo.value = "");
-    } else {
-        [nome, email, mensagem].forEach(campo => campo.style.border = "1px solid red");
-        setTimeout(() => {
-            [nome, email, mensagem].forEach(campo => campo.style.border = "none");
-        }, 1000);
-    }
+   if (nome.value.trim() !== '' && email.value.trim() !== '' && emailValido) {
+      window.location.href = linkEmail;
+      [nome, email, mensagem].forEach(campo => campo.value = "");
+   } else {
+      [nome, email, mensagem].forEach(campo => campo.style.border = "1px solid red");
+      setTimeout(() => {
+         [nome, email, mensagem].forEach(campo => campo.style.border = "none");
+      }, 1000);
+   }
 });
 
+//* Skills generator
+
+const skillList = document.getElementById('skill-list')
+
+const skills = [
+   //name, background, color
+   ['JavaScript', '#f7df1e', '#111111'],
+   ['HTML', '#e34c26', '#ffffff'],
+   ['CSS', '#9535d4ff', '#300949ff'],
+   ['React.js', '#106aa7', '#61dafb'],
+   ['Vue.js', '#42b883', '#35495e'],
+   ['Angular.js', '#dd1b16', '#ffffff'],
+   ['Node.js', '#68a063', '#ffffff'],
+   ['Tailwind', '#38bdf8', '#0f172a'],
+   ['Python', '#3776ab', '#ffd43b'],
+   ['Git & GitHub', '#e32c26', '#ffffff'],
+   ['Gsap', '#0ae448', '#111111'],
+   ['Bootstrap', '#9561fb', '#ffffffff']
+];
+
+//mostra duas vezes pro efeito de carrosel infinito
+for (let i = 1; i <= 2; i++) {
+   //mostra as skills na tela
+   skills.forEach(skill => {
+      skillList.innerHTML += `<li style="--this-bg-color: ${skill[1]}; --this-text-color: ${skill[2]}">${skill[0]}</li>`
+   })
+}
 
 //* gsap scroll animations
 
 const imgProfile = document.getElementById("img-profile")
 const aboutContent = document.getElementById("about-content")
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
-
-
-function animar(id, x, start, end) {
-    gsap.from(id, {
-        opacity: 0,
-        x: x,
-        scrollTrigger: {
-            trigger: "#about-section",
-            start: start,
-            end: end,
-            scrub: true,
-            // markers: true
-        }
-    });
+function animate(local, item, x, start, end) {
+   gsap.from(item, {
+      opacity: 0,
+      x: x,
+      scrollTrigger: {
+         trigger: local,
+         start: start,
+         end: end,
+         scrub: true,
+         // markers: true
+      }
+   });
 }
-animar(imgProfile, -50, "top 60%", "top 30%")
-animar(aboutContent, 50, "top 40%", "top 10%")
-
+animate("#about-section", imgProfile, -50, "top 60%", "top 30%")
+animate("#about-section", aboutContent, 50, "top 40%", "top 10%")
